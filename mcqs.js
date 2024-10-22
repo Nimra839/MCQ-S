630e9a6c6bcf4262b3a82358ac0f951a25f1b23e
GitWizard <nimraghouri97@gmail.com>
2024-10-22 13:22:41 +0500

    Add files via upload

---
+++ mcqs.js
@@ -0,0 +1,17 @@
+
+prompt("enter your name here")
+console.log("enter yourname here")
+prompt("enter your class here")
+console.log("enter your class here")
+prompt("enter your school name here")
+console.log("enter yourschool here")
+
+
+function checkAnswer(button, isCorrect) {
+    if (isCorrect) {  
+        alert("right answer!");
+    } else {
+        alert("wrong answer x. please try again");
+    }}
+
+  
